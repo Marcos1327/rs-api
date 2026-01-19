beb15a8f8bb96ec0ec8a108e2487ea53db27591f
Marcos Giovanny <giovannygigi@gmail.com>
2026-01-19 18:07:14 -0300

    First commit

---
+++ src/main/java/com/resolve_servicos/ResolveServicosApplication.java
@@ -0,0 +1,13 @@
+package com.resolve_servicos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResolveServicosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResolveServicosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/resolve_servicos/ResolveServicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.resolve_servicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResolveServicosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
